6ce049675f5ba555c95379f59fe20f106bbcd2f8
Long Kim <1652758@hcmut.edu.vn>
2019-12-12 17:08:24 +0700

    chore: format code

--- pages/test-modal.tsx
+++ pages/test-modal.tsx
@@ -3,52 +3,49 @@ import React from 'react';
 import BorrowModal from '../components/modal/BorrowModal';
 
 const list = [
-    {
-        fullname: 'Jim Tran',
-        uid: 'abcxyz',
-        bid: '12345'
-    },
-    {
-        fullname: 'Jim Tran',
-        uid: 'abcxyz',
-        bid: '12345'
-    },
-    {
-        fullname: 'Jim Tran',
-        uid: 'abcxyz',
-        bid: '12345'
-    },
-    {
-        fullname: 'Jim Tran',
-        uid: 'abcxyz',
-        bid: '12345'
-    },
-    {
-        fullname: 'Jim Tran',
-        uid: 'abcxyz',
-        bid: '12345'
-    },
-    {
-        fullname: 'Jim Tran',
-        uid: 'abcxyz',
-        bid: '12345'
-    },
-    
-]
+  {
+    fullname: 'Jim Tran',
+    uid: 'abcxyz',
+    bid: '12345',
+  },
+  {
+    fullname: 'Jim Tran',
+    uid: 'abcxyz',
+    bid: '12345',
+  },
+  {
+    fullname: 'Jim Tran',
+    uid: 'abcxyz',
+    bid: '12345',
+  },
+  {
+    fullname: 'Jim Tran',
+    uid: 'abcxyz',
+    bid: '12345',
+  },
+  {
+    fullname: 'Jim Tran',
+    uid: 'abcxyz',
+    bid: '12345',
+  },
+  {
+    fullname: 'Jim Tran',
+    uid: 'abcxyz',
+    bid: '12345',
+  },
+];
 
 // Styling button by passing CSS Properties
 const buttonStyle = {
-    color: 'red'
-}
+  color: 'red',
+};
 
 const TestModal: NextPage = () => {
-    return (
-        <React.Fragment>
-            <BorrowModal list={list} styles={buttonStyle}>
-
-            </BorrowModal>
-        </React.Fragment>
-    );
-} 
+  return (
+    <React.Fragment>
+      <BorrowModal list={list} styles={buttonStyle} />
+    </React.Fragment>
+  );
+};
 
 export default TestModal;
